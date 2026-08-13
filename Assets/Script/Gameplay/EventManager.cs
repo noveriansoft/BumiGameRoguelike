620615403f06d7aa33d7824ecb202cd91eda5bf7
noveriansoft <62929967+noveriansoft@users.noreply.github.com>
2026-08-13 15:15:37 +0700

    add delay from event 1 to event 2
    idk

--- Assets/Script/Gameplay/EventManager.cs
+++ Assets/Script/Gameplay/EventManager.cs
@@ -1,6 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class EventManager : MonoBehaviour
@@ -8,6 +9,7 @@ public class EventManager : MonoBehaviour
     [Header("Events")]
     public List<EventData> events;
     private EventData currentEvent;
+    [SerializeField] private GameObject eventPanel;
 
     [Header("UI")]
     [SerializeField] private TMP_Text titleText;
@@ -19,7 +21,7 @@ public class EventManager : MonoBehaviour
     private void Start()
     {
         Debug.Log("EventManager Start");
-        GenerateEvent();
+        StartCoroutine(FirstEventRoutine());
     }
 
     public void GenerateEvent()
@@ -27,24 +29,7 @@ public class EventManager : MonoBehaviour
         currentEvent = events[Random.Range(0, events.Count)];
         Debug.Log(currentEvent.eventTitle);
         UpdateUI();
-    }
-
-    public void SelectChoice(int index)
-    {
-        ChoiceData choice = currentEvent.choices[index];
-
-        ResourceManager.Instance.ModifyFuel(choice.fuelChange);
-        ResourceManager.Instance.ModifyTruckCondition(choice.truckChange);
-        ResourceManager.Instance.ModifyCargo(choice.cargoChange);
-
-        if (ResourceManager.Instance.IsDead())
-        {
-            Debug.Log("GAME OVER");
-            return;
-        }
-
-        RunManager.Instance.AdvanceStage();
-        GenerateEvent();
+        eventPanel.SetActive(true);
     }
 
     private void UpdateUI()
@@ -64,5 +49,37 @@ public class EventManager : MonoBehaviour
                 choiceButtons[i].gameObject.SetActive(false);
             }
         }
+    }
+
+    public void SelectChoice(int index)
+    {
+        ChoiceData choice = currentEvent.choices[index];
+
+        ResourceManager.Instance.ModifyFuel(choice.fuelChange);
+        ResourceManager.Instance.ModifyTruckCondition(choice.truckChange);
+        ResourceManager.Instance.ModifyCargo(choice.cargoChange);
+
+        if (ResourceManager.Instance.IsDead())
+        {
+            Debug.Log("GAME OVER");
+            return;
+        }
+
+        RunManager.Instance.AdvanceStage();
+        eventPanel.SetActive(false);
+        StartCoroutine(NextEventRoutine());
+    }
+
+    private IEnumerator NextEventRoutine()
+    {
+        yield return new WaitForSeconds(2f);
+        GenerateEvent();
+    }
+
+    private IEnumerator FirstEventRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+
+        GenerateEvent();
     }
 }
